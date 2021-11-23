9c3a2059aa2e25822afd0f5e37532f0b5ed913c7
Benjamin Benson <bensonb10@gmail.com>
2021-11-23 14:08:06 -0600

    heroku

--- config/connection.php
+++ config/connection.php
@@ -1,10 +1,11 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "myPhP";
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "root";
+    // $dbname = "myPhP";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    // $conn = new mysqli($servername, $username, $password, $dbname);
+    // if ($conn->connect_error) {
+    //     die("Connection failed: " . $conn->connect_error);
+    // }
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '11e73b99bb2d9c93aa1c9cc6061939bd9c62de2f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '11e73b99bb2d9c93aa1c9cc6061939bd9c62de2f',
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v201',
+            'version' => '201.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'reference' => 'aa5b19de5c45a32af27c87d655f5229b155fce25',
+            'dev_requirement' => true,
+        ),
+    ),
+);
