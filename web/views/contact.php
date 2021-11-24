4f2d66e6708b19c6cebce29a9ebb2f2ec99a9f26
Benjamin Benson <bensonb10@gmail.com>
2021-11-24 13:10:15 -0600

    updates

--- web/views/contact.php
+++ web/views/contact.php
@@ -1,3 +1,30 @@
+<?php
+$email = $emailErr = "";
+$message = $messageErr = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+        }
+    }
+    if (empty($_POST["message"])) {
+        $messageErr == "Message is required";
+    } else {
+        $email = test_input($_POST["message"]);
+    }
+}
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
 <main>
     <section class="products-sec">
         <h1 class="title">Contact Us</h1>
@@ -41,35 +68,36 @@
                 <h3 class="center c-title">
                     We would love to hear from you!
                 </h3>
-                <form action="" method="post" onsubmit="return validate();" id="contact">
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                     <div class="form-row">
-                        <div id="error"></div>
-                        <div id="success"></div>
-                    </div>
-                    <div class="form-row">
-                        <label for="name">Name:</label>
-                        <input type="text" id="name" placeholder="James" class="input">
+                        <div id="error">
+                            <?php
+                            echo $nameErr;
+                            if (strlen($nameErr)) {
+                                echo "<br />";
+                            }
+                            echo $messageErr;
+                            ?>
+                        </div>
                     </div>
                     <div class="form-row">
                         <label for="email">Email:</label>
-                        <input type="text" id="email" class="input" placeholder="someone@xyz.com">
-                    </div>
-                    <div class="form-row">
-                        <label for="subject">Subject:</label>
-                        <input type="text" id="subject" class="input" placeholder="Pricing">
+                        <input type="text" name="email" class="input" placeholder="someone@xyz.com" value="<?php echo $email; ?>">
                     </div>
                     <div class="form-row">
                         <label for="message">Message:</label>
-                        <textarea name="" id="message" class="input" rows="10"></textarea>
+                        <textarea name="message" class="input" rows="10">
+                        <?php echo $message; ?>
+                        </textarea>
                     </div>
                     <div class="form-row center">
                         <button class="btn" type="submit">
-                        Submit
-                    </button>
+                            Submit
+                        </button>
                     </div>
                 </form>
             </div>
         </div>
     </section>
 </main>
-<script src="assets/js/contact.js"></script>
+<!-- <script src="assets/js/contact.js"></script> -->
