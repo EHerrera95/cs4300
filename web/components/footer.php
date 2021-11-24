f9c30199998f419b2c86234c918f68e9765d1c9b
Benjamin Benson <bensonb10@gmail.com>
2021-11-24 06:05:48 -0600

    default time value

--- web/components/footer.php
+++ web/components/footer.php
@@ -38,7 +38,7 @@
         </ul>
     </div>
     <div class="sub-footer">
-        <p>Current Time: <span id="time"></span></p>
+        <p>Current Time: <span id="time">12/25/2021, 9:00:00 AM</span></p>
     </div>
 </footer>
 <script src="assets/js/social.js"></script>
