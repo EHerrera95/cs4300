c5f5c3699695c513140a99fe36636a44db041f7a
EHerrera95 <44620318+EHerrera95@users.noreply.github.com>
2021-12-01 15:00:58 -0600

    EDIT testimonials page

--- web/views/testimonials.php
+++ web/views/testimonials.php
@@ -1,5 +1,91 @@
 <main>
-    <section class="products-sec">
-        <h1 class="title">Testimonials</h1>
-    </section>
-</main>
+        <section class="products-sec">
+            <h1 class="title">Testimonials</h1>
+            <div class="testimonial">
+            <div class="small-container">
+                <div class="row">
+                    <div class="col-3">
+                        <i class="fa fa-quote-left"></i>
+                        <p>Thanks guys, keep up the good work! Buy this now. </p>
+                        <div class="rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <img src="assets/img/user1.png">
+                        <h3>Rachel Hill</h3>
+                    </div>
+                    <div class="col-3">
+                        <i class="fa fa-quote-left"></i>
+                        <p>Not able to tell you how happy I am with Jewelry.</p>
+                        <div class="rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <img src="assets/img/user2.png">
+                        <h3>Linda Davis</h3>
+                    </div>
+                    <div class="col-3">
+                        <i class="fa fa-quote-left"></i>
+                        <p>It really saves me time and effort. Jewelry is exactly what our business has been lacking.</p>
+                        <div class="rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <img src="assets/img/user3.png">
+                        <h3>Mike Smith</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3">
+                        <i class="fa fa-quote-left"></i>
+                        <p>I don't know what else to say. Man, this thing is getting better and better as I learn more about it. I like F & M's more and more each day because it makes my life a lot easier. F & M's did exactly what you said it does.</p>
+                        <div class="rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <img src="assets/img/user4.png">
+                        <h3>Sean Parker</h3>
+                    </div>
+                    <div class="col-3">
+                        <i class="fa fa-quote-left"></i>
+                        <p>We're loving it. F & M's has completely surpassed our expectations. The service was excellent.</p>
+                        <div class="rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <img src="assets/img/user5.png">
+                        <h3>Debra Brooks</h3>
+                    </div>
+                    <div class="col-3">
+                        <i class="fa fa-quote-left"></i>
+                        <p>We've used F & M's for the last five years. We've seen amazing results already.</p>
+                        <div class="rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <img src="assets/img/user6.png">
+                        <h3>Sara Reed</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+    </main>
